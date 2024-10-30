fb14505f0101e24ab80551da0900b866ce1270bc
JounQin <admin@1stg.me>
2024-10-30 18:48:39 +0800

    fix: preserve reference in .d.ts

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="../../global.d.ts" />
+/// <reference path="../../global.d.ts" preserve="true" />
 
 export * from './components/index.js'
