420ebcbea8c9774005621bc1f0dc8d1ed61bc04d
JounQin <admin@1stg.me>
2025-05-29 22:09:11 +0800

    fix: add zh locale support for algolia search

---
+++ .changeset/healthy-shirts-wink.md
@@ -0,0 +1,5 @@
+---
+"@alauda/doom": patch
+---
+
+fix: add zh locale support for algolia search
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,19 +1,32 @@
+import { useLang } from '@rspress/core/runtime'
 import { Search as OriginalSearch } from '@rspress/core/theme'
-import { Search as AlgoliaSearch } from '@rspress/plugin-algolia/runtime'
+import {
+  Search as AlgoliaSearch,
+  ZH_LOCALES,
+} from '@rspress/plugin-algolia/runtime'
+import { useMemo } from 'react'
 
 const Search =
   process.env.ALGOLIA_APP_ID &&
   process.env.ALGOLIA_API_KEY &&
   process.env.ALGOLIA_INDEX_NAME
-    ? () => (
-        <AlgoliaSearch
-          docSearchProps={{
+    ? () => {
+        const lang = useLang()
+        const docSearchProps = useMemo(
+          () => ({
             appId: process.env.ALGOLIA_APP_ID!,
             apiKey: process.env.ALGOLIA_API_KEY!,
             indexName: process.env.ALGOLIA_INDEX_NAME!,
-          }}
-        />
-      )
+            searchParameters: {
+              facetFilters: [`language:${lang}`],
+            },
+          }),
+          [lang],
+        )
+        return (
+          <AlgoliaSearch docSearchProps={docSearchProps} locales={ZH_LOCALES} />
+        )
+      }
     : OriginalSearch
 
 // eslint-disable-next-line import-x/export
