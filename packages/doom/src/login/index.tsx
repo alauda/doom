1827979dfb1daa0f0d8467effaaf19940c54dc64
JounQin <admin@1stg.me>
2025-10-29 17:40:20 +0800

    fix: improve login style
    close IDP-1345

--- packages/doom/src/login/index.tsx
+++ packages/doom/src/login/index.tsx
@@ -2,8 +2,13 @@ import { NoSSR, useSearchParams } from '@rspress/core/runtime'
 import { useCallback } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-import { LoginForm } from './LoginForm/index.tsx'
+import { X } from '../runtime/components/_X.tsx'
+import { useTranslation } from '../runtime/index.ts'
 
+import { LoginForm } from './LoginForm/index.tsx'
+import { getCloudOrigin, isIoSite } from './utils.ts'
+
+import bg from '@alauda/doom/assets/login-bg.svg'
 import classes from '@alauda/doom/styles/login.module.scss'
 
 const Login = () => {
@@ -13,9 +18,23 @@ const Login = () => {
   const onLoggedIn = useCallback(() => {
     navigate(from || '/')
   }, [from, navigate])
+  const t = useTranslation()
   return (
-    <div className={classes.container}>
-      <LoginForm onLoggedIn={onLoggedIn} />
+    <div
+      className={classes.container}
+      style={{ background: `no-repeat 0 100px url(${bg})` }}
+    >
+      <div className={classes.loginForm}>
+        <div className={classes.title}>{t('user_login')}</div>
+        <div className={classes.content}>
+          {t('user_login_tip1')}
+          <X.a href={getCloudOrigin()}>
+            {t(`customer_portal_${isIoSite() ? 'global' : 'china'}`)}
+          </X.a>
+          {t('user_login_tip2')}
+        </div>
+        <LoginForm onLoggedIn={onLoggedIn} />
+      </div>
     </div>
   )
 }
