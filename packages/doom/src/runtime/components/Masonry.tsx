0be4926d80de74e58112785e07f1a435aa6d9693
JounQin <admin@1stg.me>
2025-12-31 16:19:49 +0800

    fix: window reference on server (#234)

--- packages/doom/src/runtime/components/Masonry.tsx
+++ packages/doom/src/runtime/components/Masonry.tsx
@@ -1,8 +1,14 @@
-import type MasonryLayout from 'masonry-layout'
+import type { Options } from 'masonry-layout'
 import { useLayoutEffect, useRef, type HTMLAttributes } from 'react'
 
 export interface MasonryProps extends HTMLAttributes<HTMLDivElement> {
-  options?: MasonryLayout.Options
+  options?: Options
+}
+
+let MasonryLayout: typeof import('masonry-layout')
+
+if (typeof window !== 'undefined') {
+  MasonryLayout = (await import('masonry-layout')).default
 }
 
 export const Masonry = ({ options, ...props }: MasonryProps) => {
@@ -13,11 +19,7 @@ export const Masonry = ({ options, ...props }: MasonryProps) => {
       return
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    const masonry = new (require('masonry-layout') as typeof MasonryLayout)(
-      ref.current,
-      options,
-    )
+    const masonry = new MasonryLayout!(ref.current, options)
 
     return () => {
       masonry.destroy?.()
