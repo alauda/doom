e279147466d4256b5e4e126a44cd9cf5223de6e2
JounQin <admin@1stg.me>
2026-03-30 14:41:53 +0800

    feat: support download links
    close IDP-1457

--- packages/doom/src/theme/index.ts
+++ packages/doom/src/theme/index.ts
@@ -1,7 +1,11 @@
 /* eslint-disable import-x/export */
 
+import { X } from '../runtime/components/_X.js'
+
 export * from '@rspress/core/theme-original'
 export { EditLink } from './EditLink.js'
 export { Layout } from './Layout.js'
 export { CodeBlock } from './CodeBlock.js'
 export { Search } from './Search.js'
+
+export const getCustomMDXComponent = () => X
